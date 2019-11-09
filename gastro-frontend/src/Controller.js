4d333ddb7136304a0ba21da1b92cdc994a1f1c0c
Rene Clemens Haubitzer <haubitzer@student.tugraz.at>
2019-11-09 22:09:34 +0100

    controller

--- gastro-frontend/src/Controller.js
+++ gastro-frontend/src/Controller.js
@@ -1,172 +1,69 @@
-import React from "react";
-
-class CreateBox extends React.Component {
-
-	constructor(props) {
-        super(props);
-    }
-
-    saveBox = () => {
-    }
-
-    render() {
-
-    	}
-
-export default CreateBox;
-import {action, observable} from "mobx";
-
-
-class Store {
-    @observable var = false;
-    
-    constructor() {
-
-    }
-
-    @action setVar(newVar) {
-        this.var = newVar;
-    }
-}
-
-export default new Store();
-import React from 'react';
-import {inject, observer} from "mobx-react";
-
-
-
-@inject('Store')
-@observer
-class View extends React.Component {
-    render() {
-        const {Store} = this.props;
-
-        let display;
-
-        if (Store.var === "blablabla") {
-            display = <LoadingSpinner/>;
-        } else {
-            display = null;
-        }
-
-        return (
-            <div>
-                {display}
-            </div>
-        );
-    }
-}
-
-export default View;
-https://stackoverflow.com/questions/52641907/how-to-get-mobx-decorators-to-work-with-create-react-app-v2
-{
-  "presets": [
-    "react-app",
-    ["@babel/preset-env", { "modules": false }],
-    "@babel/preset-react"
-  ],
-  "plugins": [
-    [
-      "@babel/plugin-proposal-decorators",
-      {
-        "legacy": true
-      }
-    ],
-    [
-      "@babel/plugin-proposal-class-properties",
-      {
-        "loose": true
-      }
-    ]
-  ]
-}
-.babelrc
-@action loadBoxData(boxId, type) {
-        this.setIsLoading(true);
-        this.setDisplay(true);
-
-        axios.get(this.dataUrl, {
-            params: {
-                boxId: boxId
-            }
-        }).then(response => {
-            console.log(response.data)
-            this.boxData = [];
-            //load humidity
-            if (type === 1) {
-                response.data.map(item => {
-                    this.boxData.push({x: new Date(item.time), y: item.airHumidity});
-                });
-            } else if (type === 2) {
-                response.data.map(item => {
-                    this.boxData.push({x: new Date(item.time), y: item.airTemperature});
-                });
-            }
-
-        }).catch(err => {
-            console.error("loa…
 import axios from 'axios';
 
-class _PlantService {
-
+class _Controller {
 
     constructor() {
-        this.url = 'http://localhost:8080/plant';
-        this.testUrl = this.url + '/test';
-        this.getUrl = this.url + '/all';
-        this.createUrl = this.url + '/new';
-        this.dataUrl = this.url + '/data';
-        this.waterUrl = this.url + '/water';
-        this.updateUrl = this.url + '/update';
+        this.url = 'http://localhost:5000';
+        this.profileGeneralUrl = this.url + '/profileGeneral';
+        this.profileFoodUrl = this.url + '/profileFood';
+        this.questionUrl = this.url + '/question';
+        this.resultsUrl = this.url + '/results';
+        this.restauerantUrl = this.url + '/restaurant';
 
-        this.getTest = this.getTest.bind(this);
 
-        this.getPlants = this.getPlants.bind(this);
-        this.postPlant = this.postPlant.bind(this);
-        this.getPlantData = this.getPlantData.bind(this);
-        this.waterPlant = this.waterPlant.bind(this);
+        this.getProfileGeneral = this.getProfileGeneral.bind(this);
+        this.postProfileGeneral = this.postProfileGeneral.bind(this);
+        this.getProfileFood = this.getProfileFood.bind(this);
+        this.getQuestion = this.getQuestion.bind(this);
+        this.getResults = this.getResults.bind(this);
+        this.getRestaurant = this.getRestaurant.bind(this);
     }
 
-    getTest() {
-        axios.get(this.testUrl)
-            .then(res => {
-                console.log(res);
-            })
-    }
-
-    getPlants(box_id) {
-        return axios.get(this.getUrl, {
+    getProfileGeneral(art) {
+        return axios.get(this.profileGeneralUrl, {
             params: {
-                boxId: box_id
+                art: art
             }
-        });
+        }).then(res => res.data);
     }
 
-    postPlant(plantData) {
-        return axios.post(this.createUrl, plantData);
+    postProfileGeneral(id, art) {
+        return axios.post(this.profileGeneralUrl, {
+            id: id,
+            art: art
+        }).then(res => true);
     }
 
-    updatePlant(plantData) {
-        return axios.post(this.updateUrl, plantData);
+    getProfileFood() {
+        return axios.get(this.profileFoodUrl).then(res => res.body);
     }
 
-    getPlantData(plantId) {
-        return axios.get(this.dataUrl, {
-            params: {
-                plantId: plantId
-            }
-        });
+    postProfileFood(id, art) {
+        return axios.post(this.profileFoodUrl, {
+            id: id,
+            art: art
+        }).then(res => true);
     }
 
-    waterPlant(plantId) {
-        return axios.get(this.waterUrl, {
-            params: {
-                plantId: plantId
-            }
-        });
+    getQuestion() {
+        return axios.get(this.questionUrl).then(res => res.body);
+    }
+
+    postQuestion(id, answerId) {
+        return axios.post(this.questionUrl, {
+            id: id,
+            answerId: answerId
+        }).then(res => true);
+    }
+
+    getResults() {
+        return axios.get(this.resultsUrl).then(res => res.body);
+    }
+    getRestaurant() {
+        return axios.get(this.restauerantUrl).then(res => res.body);
     }
 }
 
-const PlantService = new _PlantService();
+const Controller = new _Controller();
 
-export default PlantService;
+export default Controller;
